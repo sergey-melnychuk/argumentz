6f5e8a09721df8476c525dbaf492a3e21c2b4ff4
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2019-12-30 18:47:22 +0100

    Add empty project from template

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertEquals(2, 1 + 1);
+    }
+}
